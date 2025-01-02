ab827b730833a5124d435ea2673c16a2522c5b26
deepak404found <deepakyadu404@gmail.com>
2025-01-02 06:17:12 +0000

    some fixes

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ export const vars = {
     PORT: process.env?.PORT ? parseInt(process.env.PORT) : 3001,
     PG_USER: process.env?.PG_USER || 'postgres',
     PG_HOST: process.env?.PG_HOST || 'localhost',
-    PG_DATABASE: process.env?.PG_DATABASE || 'chips-mis',
+    PG_DATABASE: process.env?.PG_DATABASE || 'chips',
     PG_PASSWORD: process.env?.PG_PASSWORD || 'admin@123',
     PG_PORT: process.env?.PG_PORT ? parseInt(process.env.PG_PORT) : 5432,
     PG_SSL: process.env?.PG_SSL === 'true' || false,
